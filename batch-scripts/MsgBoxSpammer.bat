54c0030bd4b20ded0f5addd6d0c88dfa98d71919
haze_e <135668188+HazeeOnYT@users.noreply.github.com>
2024-06-25 20:14:12 -0600

    Update MsgBoxSpammer.bat

--- batch-scripts/MsgBoxSpammer.bat
+++ batch-scripts/MsgBoxSpammer.bat
@@ -1,7 +1,17 @@
 @echo off
+setlocal enabledelayedexpansion
+
 :msg
+set /a x=%random% %% 800
+set /a y=%random% %% 600
+
 echo Set WshShell = WScript.CreateObject("WScript.Shell") > temp.vbs
-echo WshShell.Popup "Hello World!", 0, "Message", 64 >> temp.vbs
+echo x = %x% >> temp.vbs
+echo y = %y% >> temp.vbs
+echo Set objShell = CreateObject("Shell.Application") >> temp.vbs
+echo objShell.Popup "Hello World!", 0, "Message", 64 >> temp.vbs
+echo WshShell.Popup "Hello World!", 0, "Message", 64, x, y >> temp.vbs
+
 cscript //nologo temp.vbs
 del temp.vbs
 goto :msg
